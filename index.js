576c5c07f0e7a8faae0711ebd16047b8531a7724
triloon <dimontopcik59@gmail.com>
2025-10-03 13:44:23 +0300

    HW-6

---
+++ index.js
@@ -0,0 +1,49 @@
+// 1
+// let a = 1;
+// while (a <= 10) {
+//     console.log(a);
+//     a++;
+// }
+
+// 2
+// let a = 1;
+// while (a <= 20) {
+//     if (a % 2 === 0) {
+//         console.log(a);
+//     }
+//     a++;
+// }
+
+// for (a; a <= 20; a++){
+//     if (a % 2 === 0) {
+//         continue;
+//     }
+//     console.log(a);
+// }
+
+// 3
+// let number = 7;
+// let i = 1;
+// for (i; i <= 10; i++){
+//     console.log(`${number} * ${i} = ${number * i}`);
+// }
+
+// 4
+// let n = 15;
+// let i = 1;
+// while (true) {
+//     if (i >= n) {
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+// 5
+// let i = 1;
+// for (i; i <= 20; i++){
+//     if (i % 3 === 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
